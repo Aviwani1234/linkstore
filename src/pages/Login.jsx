a3464e4c0276720b74b3c67d84fdfdaa9c384374
Aviwani1234 <viratwani22@gmail.com>
2024-01-13 08:46:51 +0530

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { auth } from '../config/firebase';
-import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
   return (
     <div className="login">
       <form>
         <h1>Hello!</h1>
-        <intput type="text" placeholder="username" />
-        <intput type="password" placeholder="Enter Your Password" />
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Enter Your Password" />
         <button type="submit">Login</button>
       </form>
     </div>
