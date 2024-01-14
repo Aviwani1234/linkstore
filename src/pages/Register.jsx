4388426464b5c0a6fa02880b58b717b1fa0a8dac
Aviwani1234 <viratwani22@gmail.com>
2024-01-14 22:46:58 +0530

    linkstore10 --styling Login/Register pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Register.css";
 
 function Register() {
   return (
     <div className="register">
       <form>
-        <h1>Hello</h1>
+        <h1>Hello!</h1>
         <input type="text" placeholder="Enter Your First Name" />
         <input type="text" placeholder="Enter Your Last Name" />
         <input type="email" placeholder="Enter Your Email Id" />
